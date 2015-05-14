3b9572c912e6d44750284cbec27cdd0d4335d659
Oxkhar <hi@oxkhar.com>
2015-05-14 02:28:56 +0200

    feat(hook): refactor to use config variables in setup file

--- githook-pre-commit.sh
+++ githook-pre-commit.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 PATH_PWD=$(pwd)
-SCRIPT_PATH=$(cd $(dirname $0); pwd)
-SCRIPT_NAME=$(basename $0)
+SCRIPT_PATH=$(dirname $(realpath $0))
+SCRIPT_NAME=$(basename $(realpath $0))
 
-GIT_HOOK_GREP_WHITELIST_FILTER="grep -E '.*\.php$'"
-GIT_HOOK_GREP_BLACKLIST_FILTER="grep -v 'spec/'"
+. ${SCRIPT_PATH}/phpeach.conf
 
 if git rev-parse --verify HEAD >/dev/null 2>&1; then
     against=HEAD
@@ -14,13 +13,21 @@ else
     against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
 fi
 
-LIST_FILES=$(git diff-index --name-only --cached --diff-filter=AM $against | ${GIT_HOOK_GREP_WHITELIST_FILTER} | ${GIT_HOOK_GREP_BLACKLIST_FILTER})
+GITHOOK_CMD_FILES="git diff-index --name-only --cached --diff-filter=AM $against"
+if [[ "${GITHOOK_GREP_WHITELIST_FILTER}" != "" ]]; then
+	GITHOOK_CMD_FILES="${GITHOOK_CMD_FILES} | ${GITHOOK_GREP_WHITELIST_FILTER}"
+fi
+if [[ "${GITHOOK_GREP_BLACKLIST_FILTER}" != "" ]]; then
+	GITHOOK_CMD_FILES="${GITHOOK_CMD_FILES} | ${GITHOOK_GREP_BLACKLIST_FILTER}"
+fi
 
-if [[ "$LIST_FILES" == "" ]]; then
+GITHOOK_FILES=$(eval "${GITHOOK_CMD_FILES}")
+
+if [[ "$GITHOOK_FILES" == "" ]]; then
     exit 0
 fi
 
-${SCRIPT_PATH}/check-php.sh ${LIST_FILES}
+${SCRIPT_PATH}/phpeach.sh ${GITHOOK_FILES}
 IS_OK=$?
 
 exit $IS_OK
