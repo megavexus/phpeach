83e40ed35c9eb5d68b0262102e349e112db7cde5
Oxkhar <hi@oxkhar.com>
2015-10-31 04:11:11 +0100

    feat(shell): remove STATUS var and check correct status in pipelines

--- phpeach.sh
+++ phpeach.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME=$(basename $0)
 PHPEACH_TOOLS_PATH=$([ -e ${SCRIPT_PATH}/phpcs ] && echo ${SCRIPT_PATH}/)
 
 # Setup config, please...
-source ${SCRIPT_PATH}/phpeach.conf 2> /dev/null && source ${SCRIPT_PATH}/phpeach.conf.dist
+source ${SCRIPT_PATH}/phpeach.conf 2> /dev/null || source ${SCRIPT_PATH}/phpeach.conf.dist
 
 # Tools are under vendors deploy?
 if [[ "$PHPEACH_TOOLS_PATH" == "" ]]; then
@@ -27,16 +27,15 @@ CHECK_ACTIVE['tests']=1
 
 # Initialize
 STATUS_CODE=0
-SUCCESS=1
-function check_failed() { SUCCESS=0; }
-function check_is_ok() { [ ! $SUCCESS -eq 0 ]; return $?; }
+function check_failed() { STATUS_CODE=$1; }
+function check_is_ok() { [ $STATUS_CODE -eq 0 ]; return $?; }
 function check_is_active() { [ ! ${CHECK_ACTIVE[$1]} -eq 0 ]; return $?; }
 
 # Come on...
 echo -e "\n-=[ Code Quality Review ]=-\n"
 
 LIST_FILES=$*
-[ "$LIST_FILES" == "" ] && echo "No files to check" && check_failed
+[ "$LIST_FILES" == "" ] && echo "No files to check" && check_failed -1
 
 # Lint php
 if check_is_ok && check_is_active syntax; then
@@ -45,8 +44,7 @@ if check_is_ok && check_is_active syntax; then
     for PHP_SCRIPT in $LIST_FILES; do
         if ! php -l ${PHP_SCRIPT} > /dev/null; then
             echo "Errors parsing ${PHP_SCRIPT}"
-            STATUS_CODE=1
-            check_failed
+            check_failed 1
         fi
     done
 fi
@@ -56,12 +54,12 @@ if check_is_ok && check_is_active cs-fix; then
     echo -e "\nChecking code to fix..."
 
     for PHP_SCRIPT in $LIST_FILES; do
-        if ! ${PHPEACH_TOOLS_PATH}php-cs-fixer fix --dry-run --fixers=${PHPEACH_CODE_FIXERS} ${PHP_SCRIPT}; then
-            STATUS_CODE=2
-            check_failed
+        ${PHPEACH_TOOLS_PATH}php-cs-fixer fix --dry-run --fixers=${PHPEACH_CODE_FIXERS} ${PHP_SCRIPT} | grep -iv "fixed all files"
+        if [ ${PIPESTATUS[0]} -ne 0 ]; then
+            check_failed 2
         else
             echo ${PHP_SCRIPT}...Ok
-        fi | grep -iv "fixed all files"
+        fi
     done
 fi
 
@@ -71,8 +69,7 @@ if check_is_ok && check_is_active psr; then
 
     for PHP_SCRIPT in $LIST_FILES; do
         if ! ${PHPEACH_TOOLS_PATH}phpcs ${PHPEACH_CS_OPTS} --standard=${PHPEACH_CODE_STANDARD} ${PHP_SCRIPT}; then
-            STATUS_CODE=4
-            check_failed
+            check_failed 4
         fi
     done
 fi
@@ -82,10 +79,10 @@ if check_is_ok && check_is_active mess-code; then
     echo -e "\nChecking code mess..."
 
     for PHP_SCRIPT in $LIST_FILES; do
-        if ! ${PHPEACH_TOOLS_PATH}phpmd ${PHP_SCRIPT} text ${PHPEACH_MESS_RULESET}; then
-            STATUS_CODE=8
-            check_failed
-        fi  | grep -v "^$"
+        ${PHPEACH_TOOLS_PATH}phpmd ${PHP_SCRIPT} text ${PHPEACH_MESS_RULESET} | grep -v "^$"
+        if [ ${PIPESTATUS[0]} -ne 0 ]; then
+            check_failed 8
+        fi
     done
 fi
 
@@ -95,8 +92,7 @@ if check_is_ok && check_is_active tests; then
     echo -e "\nRunning tests..."
 
     if ! ${PHPEACH_TOOLS_PATH}phpunit --bootstrap ${PHPEACH_PHPUNIT_BOOTSTRAP} ${PHPEACH_PHPUNIT_OPTS} ${PHPEACH_PHPUNIT_TESTPATH}; then
-        STATUS_CODE=16
-        check_failed
+        check_failed 16
     fi
 fi
 
