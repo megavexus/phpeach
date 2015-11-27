74a5e1a1d4eea0a682222acc43b2ff16176c0927
Oxkhar <hi@oxkhar.com>
2015-11-27 02:12:16 +0100

    feat(jenkins): tuning docker image creation and jenkins launching

--- virtual/install-php-job.sh
+++ virtual/install-php-job.sh
@@ -2,5 +2,7 @@
 
 echo -e "\n\nInstalling job..."
 
+composer global require oxkhar/phpeach && rm -rf  ~/.composer/cache
+
 cat phpeach-job.xml | \
   java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job phpeach
